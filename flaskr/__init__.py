a316b84b189cf2e06ea5efcaf6a58db7dbce31c5
Hg <cozhiv@gmail.com>
2018-11-26 00:53:13 +0200

    initial

---
+++ flaskr/__init__.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    #create and configure
+    app = Flask(__name__, instance_relative_config = True)
+    app.config.from_mapping(
+        SECRET_KEY = "dev",
+        #DATABASE= os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    #print(test_config)
+    if test_config is None:
+        #load instance config, if it exists, when not in test mode
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    #check if the instance file exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # just hello world!
+    @app.route('/hello')
+    def hello():
+        return "Hello Boza!"
+
+    from . import db
+    @app.teardown_request
+    def teardown_request(response_or_exc):
+        db.db_session.remove()
+    db.init_db()
+
+    #from . import db
+    #db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    #from . import blog
+    #app.register_blueprint(blog.bp)
+    #app.add_url_rule('/','index')
+
+    return app
---
+++ flaskr/db.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    email = Column(String(120), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    #from flaskr.models import User
+    Base.metadata.create_all(bind=engine)
+
+
+#>>> from yourapplication.database import init_db
+#>>> init_db()
+#>>> from yourapplication.database import db_session
+#>>> from yourapplication.models import User
+#>>> u = User('admin', 'admin@localhost')
+#>>> db_session.add(u)
+#>>> db_session.commit()
+
+#>>> User.query.all()
+#[<User u'admin'>]
+#>>> User.query.filter(User.name == 'admin').first()
